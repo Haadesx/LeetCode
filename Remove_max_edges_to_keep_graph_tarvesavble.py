2876ea8c87dce33c494a0e31e68d3aab8ef05ed1
Hades <contactvaresh@gmail.com>
2024-06-30 08:05:29 +0530

    Update Remove_max_edges_to_keep_graph_tarvesavble.py

--- Remove_max_edges_to_keep_graph_tarvesavble.py
+++ Remove_max_edges_to_keep_graph_tarvesavble.py
@@ -122,8 +122,4 @@ class Solution(object):
         
         return removable_edges
 
-# Example test cases
-solution = Solution()
-print(solution.maxNumEdgesToRemove(4, [[3,1,2],[3,2,3],[1,1,3],[1,2,4],[1,1,2],[2,3,4]]))  # Output: 2
-print(solution.maxNumEdgesToRemove(4, [[3,1,2],[3,2,3],[1,1,4],[2,1,4]]))  # Output: 0
-print(solution.maxNumEdgesToRemove(4, [[3,2,3],[1,1
+
